37d9ae285fac3884134f84e3e58fe5b788f0017f
coms <oleg.chubaryov@gmail.com>
2014-04-14 01:03:01 +0400

    added small test for simple case

--- eulerProject/src/euler/Problem24.java
+++ eulerProject/src/euler/Problem24.java
@@ -3,6 +3,17 @@ package euler;
 import euler.utils.Permutation;
 
 public class Problem24 {
+	
+	/*
+	 * test sequence for lexicographical order 0,1,2
+	 * 
+	 * 012
+	 * 021
+	 * 102
+	 * 120
+	 * 201
+	 * 210
+	 */
 
 	public static void main(String[] args) {
 		
@@ -10,6 +21,10 @@ public class Problem24 {
 		Permutation perm = new Permutation();
 		char[] array = {'0','1','2'};
 		perm.createArray(array);
+		System.out.println(array);
+		perm.nextPermutation();
+		System.out.println(array);
+		perm.nextPermutation();
 		System.out.println(array);
 		
 	}
--- eulerProject/src/euler/utils/Permutation.java
+++ eulerProject/src/euler/utils/Permutation.java
@@ -17,4 +17,21 @@ public class Permutation {
 		return array.toString();
 	}
 	
+	public void nextPermutation() {
+		int l = array.length - 1;
+		boolean permutated = false;
+		
+		while(!permutated) {
+			if (array[l - 1] < array[l]) {
+				// swap l with l-1
+				char a = array[l];
+				array[l] = array[l-1];
+				array[l-1] = a;
+				permutated = true;
+			}
+			l--;
+		}
+		
+	}
+	
 }
