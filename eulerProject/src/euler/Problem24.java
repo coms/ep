3736f62daa8b6de7f8d30c719992f0e678100e61
coms <oleg.chubaryov@gmail.com>
2014-04-13 22:04:16 +0400

    initial files for Permutation problem

---
+++ eulerProject/src/euler/Problem24.java
@@ -0,0 +1,17 @@
+package euler;
+
+import euler.utils.Permutation;
+
+public class Problem24 {
+
+	public static void main(String[] args) {
+		
+		// create test permutation array
+		Permutation perm = new Permutation();
+		char[] array = {'0','1','2'};
+		perm.createArray(array);
+		System.out.println(array);
+		
+	}
+
+}
---
+++ eulerProject/src/euler/utils/Permutation.java
@@ -0,0 +1,20 @@
+package euler.utils;
+
+public class Permutation {
+
+	private char[] array;
+	
+	/**
+	 * create permutation array
+	 * 
+	 * @param array
+	 */
+	public void createArray(char[] array) {
+		this.array = array;
+	}
+	
+	public String getArray() {
+		return array.toString();
+	}
+	
+}
