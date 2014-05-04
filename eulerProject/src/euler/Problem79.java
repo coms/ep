e558603d230da88dc53e212d6aa40e9e34f1fb8a
coms <oleg.chubaryov@gmail.com>
2014-05-04 19:30:22 +0400

    added skeleton and data for problem 79

---
+++ eulerProject/src/euler/Problem79.java
@@ -0,0 +1,33 @@
+package euler;
+
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import euler.utils.DigitUtil;
+import euler.utils.E;
+import euler.utils.Fraction;
+import euler.utils.PowHelper;
+import euler.utils.PrimeNumberUtils;
+
+
+/**
+* Passcode derivation
+*  Problem 79
+* A common security method used for online banking is to ask the user 
+* for three random characters from a passcode. For example, if the passcode was 531278, 
+* they may ask for the 2nd, 3rd, and 5th characters; the expected reply would be: 317.
+* 
+* The text file, keylog.txt, contains fifty successful login attempts.
+* 
+* Given that the three characters are always asked for in order, 
+* analyse the file so as to determine the shortest possible secret passcode of unknown length.
+*/
+public class Problem79 {
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+	
+	public static void main(String[] args) {
+		
+	}
+}
