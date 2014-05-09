059320ea3dbdfb4e6178bd5bccfa149e5adc3aac
coms <oleg.chubaryov@gmail.com>
2014-05-09 10:27:48 +0400

    added solution for 79-th problem

--- eulerProject/src/euler/Problem79.java
+++ eulerProject/src/euler/Problem79.java
@@ -10,8 +10,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 import euler.utils.AlphabetHelper;
+import euler.utils.ArraysHelper;
 import euler.utils.DigitUtil;
 import euler.utils.E;
 import euler.utils.Fraction;
@@ -37,27 +42,48 @@ public class Problem79 {
 	
 	public static void main(String[] args) {
 		try {
-			ArrayList<String> names = new ArrayList<String>();
-			BufferedReader reader = new BufferedReader( new FileReader(new File("data/keycodes.txt")));
-			String str = reader.readLine();
-			String[] namesArray = str.split(",");
-			for (String rawName : namesArray) {
-				String name = rawName.substring(1, rawName.length() - 1);
-//				System.out.println(name);
-				names.add(name);
+			Set<String> codes = new HashSet<String>();
+			Set<String> sortedCodes = new ConcurrentSkipListSet<String>();
+			BufferedReader reader = new BufferedReader( new FileReader(new File("data/keylog.txt")));
+			String str;
+			while ((str = reader.readLine()) != null) {
+				codes.add(str);
 			}
-			Collections.sort(names);
-			System.out.println(names);
+			sortedCodes = ArraysHelper.sort(codes);			
 			long sum = 0l;
-			for (int i = 1; i <= names.size(); i++) {
-				String name = names.get(i - 1);
-				sum += AlphabetHelper.score(name) * i;
-			}
-			System.out.println(sum);
+			
+			String s = "73162890";
+			
+			boolean check = checkPattern(s, sortedCodes);
+			
+			System.out.println(check);
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}		
 	}
+	
+	
+	/***
+	 * Checks String s on patterns.
+	 * @param s
+	 * @param patterns
+	 * @return
+	 */
+	public static boolean checkPattern(String s, Set<String> patterns) {
+		for (String pattern : patterns) {
+			char s1 = pattern.charAt(0);
+			char s2 = pattern.charAt(1);
+			char s3 = pattern.charAt(2);
+			String p = "\\d*" + s1 + "\\d*" + s2 + "\\d*" + s3 + "\\d*";
+			if (!s.matches(p)) {
+				System.out.println(s + " is not match " + p);
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }
--- eulerProject/src/euler/utils/ArraysHelper.java
+++ eulerProject/src/euler/utils/ArraysHelper.java
@@ -1,5 +1,9 @@
 package euler.utils;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ArraysHelper {
 
 	/**
@@ -14,5 +18,15 @@ public class ArraysHelper {
 		}
 		return retval;
 	}
+	
+	public static Set<String> sort(Set<String> set) {
+		Set<String> retval = new HashSet<String>();
+		String[] oset = set.toArray(new String[0]);
+		Arrays.sort(oset);
+		System.out.println(Arrays.toString(oset));
+		retval.addAll(Arrays.asList(oset));
+		return retval;
+	}
+	
 
 }
