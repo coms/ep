bfff08654f120c39dbfc8d89f63241fd6e2bdf24
coms <oleg.chubaryov@gmail.com>
2014-05-09 10:32:10 +0400

    added skelet for 89-th problem

--- eulerProject/src/euler/Problem89.java
+++ eulerProject/src/euler/Problem89.java
@@ -14,18 +14,29 @@ import euler.utils.ArraysHelper;
 
 
 /**
-* Passcode derivation
-*  Problem 79
-* A common security method used for online banking is to ask the user 
-* for three random characters from a passcode. For example, if the passcode was 531278, 
-* they may ask for the 2nd, 3rd, and 5th characters; the expected reply would be: 317.
+* Roman numerals
+* Problem 89
+* The rules for writing Roman numerals allow for many ways of writing each number 
+* (see About Roman Numerals...). However, there is always a "best" way of writing a particular number.
+* For example, the following represent all of the legitimate ways of writing the number sixteen:
+* IIIIIIIIIIIIIIII
+* VIIIIIIIIIII
+* VVIIIIII
+* XIIIIII
+* VVVI
+* XVI
+* The last example being considered the most efficient, as it uses the least number of numerals.
+* The 11K text file, roman.txt (right click and 'Save Link/Target As...'), contains one 
+* thousand numbers written in valid, but not necessarily minimal, Roman numerals; that is, 
+* they are arranged in descending units and obey the subtractive pair rule 
+* (see About Roman Numerals... for the definitive rules for this problem).
 * 
-* The text file, keylog.txt, contains fifty successful login attempts.
+* Find the number of characters saved by writing each of these in their minimal form.
 * 
-* Given that the three characters are always asked for in order, 
-* analyse the file so as to determine the shortest possible secret passcode of unknown length.
-*/
-public class Problem79 {
+* Note: You can assume that all the Roman numerals in the file contain no more than 
+* four consecutive identical units.*/
+
+public class Problem89 {
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
 	
