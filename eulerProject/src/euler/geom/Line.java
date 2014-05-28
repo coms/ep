4bb7e9b0f9e82d081641e4ebaa3d2f368b702a32
coms <oleg.chubaryov@gmail.com>
2014-05-28 08:43:23 +0400

    added geom package

---
+++ eulerProject/src/euler/geom/Line.java
@@ -0,0 +1,42 @@
+package euler.geom;
+
+import java.math.BigDecimal;
+
+public class Line {
+
+	private Point p1 = null;
+	private Point p2 = null;
+	
+	private BigDecimal k = null;
+	private BigDecimal b = null;
+	
+	/**
+	 * Create line by 2 points.
+	 */
+	public Line(Point p1, Point p2) {
+		super();
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+	
+	/**
+	 * Create line by y = k*x + b
+	 * @param k
+	 * @param b
+	 */
+	public Line(BigDecimal k, BigDecimal b) {
+		super();
+		this.k = k;
+		this.b = b;
+	}
+
+	/**
+	 * @return  slope of line in degrees
+	 */
+	public double slope() {
+		return Math.toDegrees(Math.atan(k.doubleValue()));
+	}
+	
+	
+	
+}
--- eulerProject/src/euler/geom/Point.java
+++ eulerProject/src/euler/geom/Point.java
@@ -12,5 +12,21 @@ public class Point {
 		this.x = x;
 		this.y = y;
 	}
+
+	public BigDecimal getX() {
+		return x;
+	}
+
+	public void setX(BigDecimal x) {
+		this.x = x;
+	}
+
+	public BigDecimal getY() {
+		return y;
+	}
+
+	public void setY(BigDecimal y) {
+		this.y = y;
+	}
 	
 }
