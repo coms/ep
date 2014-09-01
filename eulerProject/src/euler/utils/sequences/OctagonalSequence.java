4208e41d88035553ffdbd5590c24ab4e706f31a6
Oleg Chubaryov <oleg.chubaryov@gmail.com>
2014-09-01 15:28:26 +0400

    prob 61 bugfix

--- eulerProject/src/euler/utils/sequences/OctagonalSequence.java
+++ eulerProject/src/euler/utils/sequences/OctagonalSequence.java
@@ -8,7 +8,7 @@ public class OctagonalSequence extends BaseSequence {
 	public BigInteger next() {
 		super.currentIndex++;
 		BigInteger n = BigInteger.valueOf(super.currentIndex);
-		BigInteger retval = n.multiply(BigInteger.valueOf(3).multiply(n).subtract(BigInteger.valueOf(3))); 
+		BigInteger retval = n.multiply(BigInteger.valueOf(3).multiply(n).subtract(BigInteger.valueOf(2))); 
 		return retval;
 	}
 
