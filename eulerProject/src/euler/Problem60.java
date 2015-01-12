fee44a5c0085cd815270899fe06bb4c250918aa0
Oleg Chubaryov <oleg.chubaryov@gmail.com>
2015-01-12 11:53:37 +0400

    added solution for problem 60

--- eulerProject/src/euler/Problem60.java
+++ eulerProject/src/euler/Problem60.java
@@ -1,6 +1,8 @@
 package euler;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import euler.utils.Prime;
@@ -18,7 +20,9 @@ public class Problem60 {
 		
 	public static void main(String[] args) {
 		System.out.println("Solve problem 60");
-		Set<Long> primes = new HashSet<Long>();
+		List<Set<Long>> chains = new ArrayList();  
+		Set<Long> chain1 = new HashSet<Long>();
+		chains.add(chain1);
 		for (long n = 11; n < 100000000; n++) {
 			if (Prime.isPrimeFaster(n)) {
 				String sn = String.valueOf(n);
@@ -29,34 +33,44 @@ public class Problem60 {
 					}
 					Long n2 = Long.valueOf(sn.substring(i));
 					Long n3 = Long.valueOf(sn.substring(i) + sn.substring(0, i));
+					Set<Long> newChain = null;
 					if (Prime.isPrimeFaster(n1) && Prime.isPrimeFaster(n2) && Prime.isPrimeFaster(n3)) {
 						Long newPrime = n1;
-						if (primes.size() < 2) {
-							primes.add(n1);
-							primes.add(n2);
-						} else if (primes.contains(n1)) {
-							newPrime = n2;
-						}
-						boolean isAddable = true; 
-						for (Long a : primes) {
-							Long n4 = Long.valueOf(a.toString() + newPrime.toString());
-							Long n5 = Long.valueOf(newPrime.toString() + a.toString());
-							if (!Prime.isPrimeFaster(n4) || !Prime.isPrimeFaster(n5)) {
-								isAddable = false;
+						for (Set<Long> primes : chains) {
+							if (primes.size() < 2) {
+								primes.add(n1);
+								primes.add(n2);
+							} else if (primes.contains(n1)) {
+								newPrime = n2;
+							}
+							boolean isAddable = true; 
+							for (Long a : primes) {
+								Long n4 = Long.valueOf(a.toString() + newPrime.toString());
+								Long n5 = Long.valueOf(newPrime.toString() + a.toString());
+								if (!Prime.isPrimeFaster(n4) || !Prime.isPrimeFaster(n5)) {
+									isAddable = false;
+								}
+							}
+							if (isAddable) {
+								primes.add(newPrime);
+//								System.out.println(chains);
+								if (primes.size() == 5) {
+									System.out.println(primes);
+									System.exit(0);
+								}
+							} else {
+								newChain = new HashSet<Long>();
+								newChain.add(n1);
+								newChain.add(n2);
 							}
 						}
-						if (isAddable) {
-							primes.add(newPrime);
-							if (primes.size() == 5) {
-								System.out.println(primes);
-								break;
-							}
+						if (newChain != null) {
+							chains.add(newChain);
 						}
 					}
 				}
 			}
 		}
-		System.out.println(primes);
 	}		
 
 }
